ae64381f50f284e30b97ced74746bf808135c3cd
WizzyGeek <51919967+WizzyGeek@users.noreply.github.com>
2021-01-18 22:40:16 +0530

    🎯 Add LICENSE and README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# Multiplication Trainer
+
+A simple program, to motivate you to do your time tables
+
+## CLI
+
+The CLI is very simple (for now), it only takes one optional number, which limits the multiplicand to that value
+
+## Why?
+
+During the pandemic, my time tables got noticably rusty, and I could not find any interactive ways to practice, and randomly generated workbooks seemed a bit too much. So in my spare time I wrote this script
+
+## Contribution
+
+**All Contributions are welcome**, feel free to add as many features as you want!
+Go Crazy!
--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import sys
 from random import choice
+from time import time
 
 def parse_args(argv=sys.argv):
     try:
@@ -15,24 +16,33 @@ def main(argv=None):
     nums = tuple(range(argv))
 
     score = 0
+    start = time()
+
+    ques = 0
 
     while True:
         num1 = choice(nums)
         num2 = choice(nums)
 
-        print(f"{num1}x{num2}")
+        print(f"{num1} x {num2}")
 
         try:
             ans = int(input(">>> Answer: "))
         except Exception:
             break
+        else:
+            ques += 1
 
         if ans == (num1 * num2):
             score += 1
-        else:
-            score -= 1
+
+    end = time()
 
     print(f"Your score was {score}")
+    print(f"You took {(end - start):2f} seconds for {ques} questions")
+    print(f"Your avg. speed was, {(ques / (end - start)):2f} questions per second or {((end - start) / ques)} seconds per question")
+    print(f"Accuracy {(score/ques):%} :)")
+    print("*You Can Do Better!*")
     input()
     return 0
 
